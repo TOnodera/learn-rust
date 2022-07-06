7830dbe4ed1e60fbedd1490365c44e7d78c51ef0
t.onodera <t.onodera.1.2.1.5@gmail.com>
2022-07-06 12:11:34 +0000

    VSコード環境セットアップ

---
+++ Box/target/debug/deps/Box-574549c811f55609.d
@@ -0,0 +1,5 @@
+/workspaces/learn-rust/Box/target/debug/deps/Box-574549c811f55609.rmeta: src/main.rs
+
+/workspaces/learn-rust/Box/target/debug/deps/Box-574549c811f55609.d: src/main.rs
+
+src/main.rs:
--- Dockerfile
+++ Dockerfile
@@ -1,4 +1,5 @@
 FROM rust:latest
 
 RUN useradd -m -u 1000 rust
+RUN rustup component add rustfmt
 USER rust
