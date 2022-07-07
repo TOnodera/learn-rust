02574ab48cca23a56dd21277ec1ff0c0446459ee
t.onodera <t.onodera.1.2.1.5@gmail.com>
2022-07-07 10:46:42 +0000

    オブジェクト指向のページ用

--- threads/src/main.rs
+++ threads/src/main.rs
@@ -1,14 +1,25 @@
 use std::sync::mpsc;
 use std::thread;
+use std::time::Duration;
 
 fn main() {
     let (tx, rx) = mpsc::channel();
 
     thread::spawn(move || {
-        let val = String::from("hi");
-        tx.send(val).unwrap();
+        let vals = vec![
+            String::from("hi"),
+            String::from("from"),
+            String::from("the"),
+            String::from("thread"),
+        ];
+
+        for val in vals {
+            tx.send(val).unwrap();
+            thread::sleep(Duration::from_millis(1));
+        }
     });
 
-    let recieved = rx.recv().unwrap();
-    println!("Got: {}", recieved);
+    for recieved in rx {
+        print!("Got: {}", recieved);
+    }
 }
