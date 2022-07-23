70f2b8d1397093e680474a5f2d6d5212159c2f83
t.onodera <t.onodera.1.2.1.5@gmail.com>
2022-07-23 22:06:00 +0000

    写経

--- Dockerfile
+++ Dockerfile
@@ -1,5 +1,5 @@
 FROM rust:latest
 
-RUN useradd -m -u 1000 rust
+RUN useradd -m -u 1000 -s /bin/bash rust 
 RUN rustup component add rustfmt
 USER rust
---
+++ bmi/src/main.rs
@@ -0,0 +1,25 @@
+use std::{collections::HashMap, hash::Hash};
+fn main() {
+    let tuki = [
+        "睦月",
+        "如月",
+        "弥生",
+        "卯月",
+        "皐月",
+        "水無月",
+        "文月",
+        "葉月",
+        "長月",
+        "神無月",
+        "霜月",
+        "師走",
+    ];
+    let mut tuki_map: HashMap<&str, usize> = HashMap::new();
+    for (i, v) in tuki.iter().enumerate() {
+        tuki_map.insert(v, i + 1);
+    }
+
+    for (key, val) in tuki_map.iter() {
+        println!("{}{}", key, val);
+    }
+}
