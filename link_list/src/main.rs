d54c4a26f5caab0245b05879ad87a5bf0756c8df
t.onodera <t.onodera.1.2.1.5@gmail.com>
2022-07-30 02:50:08 +0000

    Enumで空を表現

--- link_list/src/main.rs
+++ link_list/src/main.rs
@@ -1,20 +1,31 @@
-struct Node {
-    data: i64,
-    link: Option<Box<Node>>,
+enum Node {
+    Empty,
+    Cons(i64, Box<Node>),
 }
 
-fn node(v: i64, link: Option<Box<Node>>) -> Option<Box<Node>> {
-    Some(Box::new(Node { data: v, link }))
+use Node::{Cons, Empty};
+
+fn node(v: i64, link: Box<Node>) -> Box<Node> {
+    Box::new(Cons(v, link))
 }
 
 fn main() {
-    let c = node(10, node(20, node(30, None))).unwrap();
-    let mut p = &c;
+    let c = node(10, node(20, node(30, Box::new(Empty))));
+
+    let mut ptr = &c;
     loop {
-        println!("{}", p.data);
-        match p.link {
-            None => break,
-            Some(ref link) => p = link,
+        // ptr -> &Box<Node>
+        // *ptr -> Box<Node>
+        // **ptr -> Node
+        // &**ptr -> &Node
+        // ↓は↑の操作をやってる（Nodeの参照を取得してる）
+        let cur_node = &**ptr;
+        match cur_node {
+            Empty => break,
+            Cons(v, link) => {
+                println!("{}", v);
+                ptr = &link;
+            }
         }
     }
 }
